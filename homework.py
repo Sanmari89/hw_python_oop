72f05da71ee666dd7882ce641976169d0d220ab5
Sanmari89 <your_email@whatever.com>
2021-02-15 19:53:29 +0300

    [upd after first review(vs3)]

--- homework.py
+++ homework.py
@@ -11,12 +11,10 @@ class Calculator:
 
     def get_week_stats(self):
         today = dt.datetime.now().date()
-        date_list = [today - dt.timedelta(days=x) for x in range(7)]
         result = 0
         for record in self.records:
-            for date in date_list:
-                if record.date == date:
-                    result += record.amount
+            if today - dt.timedelta(weeks=1) <= record.date <= today:
+                result += record.amount
         return result
 
     def get_today_stats(self):
@@ -27,14 +25,18 @@ class Calculator:
                 count += record.amount
         return count
 
+    def diff(self, today_stats):
+        return self.limit - today_stats
+
 
 class Record:
-    def __init__(self, amount, comment, date=''):
-        date_format = '%d.%m.%Y'
-        if date == '':
+    date_format = '%d.%m.%Y'
+
+    def __init__(self, amount, comment, date=None):
+        if date is None:
             date = dt.datetime.now().date()
-        if type(date) == str:
-            date = dt.datetime.strptime(date, date_format).date()
+        else:
+            date = dt.datetime.strptime(date, Record.date_format).date()
         self.amount = amount
         self.comment = comment
         self.date = date
@@ -42,10 +44,11 @@ class Record:
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
-        if self.limit <= self.get_today_stats():
+        today_stats = self.get_today_stats()
+        if self.limit <= today_stats:
             return 'Хватит есть!'
 
-        diff = self.limit - self.get_today_stats()
+        diff = self.diff(today_stats)
 
         if diff > 0:
             return ('Сегодня можно съесть что-нибудь ещё, '
@@ -57,18 +60,21 @@ class CashCalculator(Calculator):
     EURO_RATE = 91.0
 
     def get_today_cash_remained(self, currency):
+        today_stats = self.get_today_stats()
+
+        if self.limit == today_stats:
+            return 'Денег нет, держись'
+
         currency_dict = {'usd': (CashCalculator.USD_RATE, 'USD'),
                          'eur': (CashCalculator.EURO_RATE, 'Euro'),
                          'rub': (1, 'руб')}
 
-        if self.limit == self.get_today_stats():
-            return 'Денег нет, держись'
-
-        diff = self.limit - self.get_today_stats()
+        diff = self.diff(today_stats)
         value = abs(round(diff / currency_dict[currency][0], 2))
+        currency = currency_dict[currency][1]
 
         if diff > 0:
-            return f'На сегодня осталось {value} {currency_dict[currency][1]}'
-        else:
-            return (f'Денег нет, держись: твой долг - {value} '
-                    f'{currency_dict[currency][1]}')
+            return f'На сегодня осталось {value} {currency}'
+
+        return (f'Денег нет, держись: твой долг - {value} '
+                    f'{currency}')
